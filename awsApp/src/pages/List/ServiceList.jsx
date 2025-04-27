63c6649c982f77c8ebbbf961132506f06178ee89
111062118 <jiangdumpling@gmail.com>
2025-04-27 10:19:12 +0800

    fix roll error

--- awsApp/src/pages/List/ServiceList.jsx
+++ awsApp/src/pages/List/ServiceList.jsx
@@ -2,44 +2,43 @@ import "../../components/List/ServiceList.css";
 import { CiSearch } from "react-icons/ci";
 import avatar from "../../assets/img/avatar.png";
 
-
 const ServiceList = () => {
   return (
-  <div>
-   <div className="ServiceList">
-    <div className="search">
-      <div className="searchBar">
-        <CiSearch />
-        <input type="text" placeholder="Search" />
+    <div>
+      <div className="ServiceList">
+        <div className="search">
+          <div className="searchBar">
+            <CiSearch />
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div className="searchitem">
+          <div className="item">
+            <img src={avatar} alt="avatar" />
+            <div className="texts">
+              <span>Jane Doe</span>
+              <p>Hello everyone!</p>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src={avatar} alt="avatar" />
+            <div className="texts">
+              <span>Jane Doe</span>
+              <p>Hello everyone!</p>
+            </div>
+          </div>
+
+          <div className="item">
+            <img src={avatar} alt="avatar" />
+            <div className="texts">
+              <span>Jane Doe</span>
+              <p>Hello everyone!</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div className="item">
-      <img src={avatar} alt="avatar" />
-      <div className="texts">
-        <span>Jane Doe</span>
-        <p>Hello everyone!</p>
-      </div>
-    </div>
-
-    <div className="item">
-      <img src={avatar} alt="avatar" />
-      <div className="texts">
-        <span>Jane Doe</span>
-        <p>Hello everyone!</p>
-      </div>
-    </div>
-
-    <div className="item">
-      <img src={avatar} alt="avatar" />
-      <div className="texts">
-        <span>Jane Doe</span>
-        <p>Hello everyone!</p>
-      </div>
-    </div>
-  </div>
-
-  </div>
   );
 };
 
